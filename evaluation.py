97a8313593fc600f109b5c7773b124bc72b25c83
Derekmod <derekmod@gmail.com>
2017-12-18 22:45:22 -0500

    fix

--- evaluation.py
+++ evaluation.py
@@ -91,9 +91,9 @@ def generatePredictions(spec, record_name):
 
     format_string = 'python inference.py --outfule_file={} --input_data_pattern={} --train_dir={}'
     command = format_string.format(TEMP_FILENAME, record_name, model_dir)
-    pid = os.system(command)
-    #print 'status of "%s" = %d' % (command, os.system(command))
-    os.waitpid(pid, 0)
+    #pid = os.system(command)
+    print 'status of "%s" = %d' % (command, os.system(command))
+    #os.waitpid(pid, 0)
 
 def getGroundTruths(filename): #verified
     '''

306d27695915b4bd95964d53706e4ad083598964
Dan Smillie <dansmillie96@gmail.com>
2017-12-19 16:16:00 -0500

    final commitgit statusgit statusgit status

--- evalModels.py
+++ evalModels.py
@@ -13,4 +13,4 @@ args = parser.parse_args()
 spec = ModelSpec(False, step=args.step, parent_dir='../frame_model_long')
 
 evaluation.copyModel(spec, '../tempModel')
-os.system("python2 ../youtube-8m/eval.py --eval_data_pattern='../frame-features/validate*.tfrecord' --train_dir=../tempModel --run_once=True --frame_features=True --model=FrameLevelLogisticModel --feature_names=rgb --feature_sizes=1024" )
+os.system("python2 ../youtube-8m/eval.py --eval_data_pattern='../features/train*.tfrecord' --train_dir=../tempModel --run_once=True --model=LogisticModel")
